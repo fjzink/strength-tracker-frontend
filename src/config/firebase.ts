deab29422bf49faf728b831873ae70173d601c9b
Fulton Zink <joeyzink@gmail.com>
2020-10-05 16:42:56 -0700

    Set up react app and add firebase sdk.

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
+export const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  };
