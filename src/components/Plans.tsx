9a3f719ec531b3cdefa2ff5345e569be157bf9cf
Fulton Zink <joeyzink@gmail.com>
2020-11-24 20:06:31 -0800

    Add effect to fetch training plans.

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { apiClient } from '../config/axios';
+
+interface RootState {
+    token: string;
+}
 
 const Plans = () => {
-    return <div>Plans</div>;
+    const token = useSelector((state: RootState) => state.token);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await apiClient.get('/api/trainingplans', {
+                headers: {
+                    Authorization: `bearer ${token}`,
+                },
+            });
+            console.log(res.data);
+        };
+
+        fetchData();
+    }, [token]);
+
+    return (
+        <div>
+            <p>Plans</p>
+        </div>
+    );
 };
 
 export { Plans };
